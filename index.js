df312759cb354c270d2cd39fa89349348c8f0afd
Devin Halladay <devin@devinhalladay.com>
2019-03-03 03:02:48 -0500

    Last try with choo, rip

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var css = require('sheetify')
 var choo = require('choo')
+require("babel-core/register");
+require("babel-polyfill");
+
 
 css('tachyons')
 
@@ -10,9 +13,10 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-service-worker')())
 }
 
-app.use(require('./stores/clicks'))
+app.use(require('./stores/store'))
 
 app.route('/', require('./views/main'))
-app.route('/*', require('./views/404'))
+app.route('/:root', require('./views/page'))
+// app.route('/*', require('./views/404'))
 
 module.exports = app.mount('body')
